b1e0f6c4192d2c2fc99bb6629c316c34cf29afa0
LucasNGRE <lucas12negre@gmail.com>
2026-04-03 09:20:39 +0200

    feat: simplify character fetching by removing pagination logic

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -3,17 +3,11 @@ import 'package:http/http.dart' as http;
 
 printRmCharacters() async {
   try {
-    String? nextUrl = 'https://rickandmortyapi.com/api/character';
+    final response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
+    final data = json.decode(response.body);
 
-    while (nextUrl != null) {
-      final response = await http.get(Uri.parse(nextUrl));
-      final data = json.decode(response.body);
-
-      for (var character in data['results']) {
-        print(character['name']);
-      }
-
-      nextUrl = data['info']['next'];
+    for (var character in data['results']) {
+      print(character['name']);
     }
   } catch (err) {
     return 'error caught: $err';
